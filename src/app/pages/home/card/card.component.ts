b1d0360dea0397a181afd52d215084d1e4f492ca
sabrina-boby <sabrinaboby786@gmail.com>
2024-04-25 17:12:49 +0600

    update from office2

--- src/app/pages/home/card/card.component.ts
+++ src/app/pages/home/card/card.component.ts
@@ -7,9 +7,13 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
     products:any = [
-      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
-      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00',rating:'../../../assets/images/brand/png/star.png',store:'../../../assets/images/brand/png/store.png' },
-      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00',rating:'../../../assets/images/brand/png/star.png',store:'../../../assets/images/brand/png/store.png' },
-      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00',rating:'../../../assets/images/brand/png/star.png',store:'../../../assets/images/brand/png/store.png' },
-      {image:'../../../assets/images/home/Frame 1000008716 (1).png', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'}      ]
+      {image:'../../../assets/images/home/Olevs-8697-Tungsten-Steel-Copper-Dial-Ladies-Watch-Gold-1.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
+      {image:'../../../assets/images/home/6JqNIG41Citz4GZwhiotfOYPxJXqIeexdj3ZHtIM.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      
+      {image:'../../../assets/images/home/Screenshot_5.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
+      {image:'../../../assets/images/home/OLEVS-5877-Ladies-Watch-Women.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      
+      {image:'../../../assets/images/home/s-l1200.webp', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
+      {image:'../../../assets/images/home/Carlington_elite_analog_ladies_watch_CT_2007_roseblack.webp', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      
+      {image:'../../../assets/images/home/1601128362583-13-originnm80prcnt-500x500.webp', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
+      {image:'../../../assets/images/home/9bfd9ddf926cc999dc4c4f7dbad7ff63.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'}      
+    ]
     }
--- src/app/pages/home/section5/section5.component.ts
+++ src/app/pages/home/section5/section5.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./section5.component.scss']
 })
 export class Section5Component {
-
+  img_gellary:any = [
+    {image:'../../../../assets/images/home/Screenshot_5.jpg'},
+    {image:'../../../../assets/images/home/download.jpg'},      
+    {image:'../../../../assets/images/home/6JqNIG41Citz4GZwhiotfOYPxJXqIeexdj3ZHtIM.jpg'},
+    {image:'../../../../assets/images/home/s-l1200.webp'},      
+    {image:'../../../../assets/images/home/OLEVS-5877-Ladies-Watch-Women.jpg'},
+    {image:'../../../../assets/images/home/Carlington_elite_analog_ladies_watch_CT_2007_roseblack.webp'},          
+  ]
 }
