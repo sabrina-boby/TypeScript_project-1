d884af701d75c644f81c9b4c119c37fc3423bc21
sabrina-boby <sabrinaboby786@gmail.com>
2024-04-25 22:45:34 +0600

    update from home again

--- src/app/pages/home/card/card.component.ts
+++ src/app/pages/home/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PRODUCTS_DB } from 'src/app/core/db/products.db';
 
 @Component({
   selector: 'app-card',
@@ -6,14 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
-    products:any = [
-      {image:'../../../assets/images/home/Olevs-8697-Tungsten-Steel-Copper-Dial-Ladies-Watch-Gold-1.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
-      {image:'../../../assets/images/home/6JqNIG41Citz4GZwhiotfOYPxJXqIeexdj3ZHtIM.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      
-      {image:'../../../assets/images/home/Screenshot_5.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
-      {image:'../../../assets/images/home/OLEVS-5877-Ladies-Watch-Women.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      
-      {image:'../../../assets/images/home/s-l1200.webp', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
-      {image:'../../../assets/images/home/Carlington_elite_analog_ladies_watch_CT_2007_roseblack.webp', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},      
-      {image:'../../../assets/images/home/1601128362583-13-originnm80prcnt-500x500.webp', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'},
-      {image:'../../../assets/images/home/9bfd9ddf926cc999dc4c4f7dbad7ff63.jpg', name:'Bollard Lamp',nPrice:'$7.00',oPrice:'$12.00'}      
-    ]
+    products:any[] = PRODUCTS_DB;
     }
